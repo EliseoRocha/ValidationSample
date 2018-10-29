893388e49634e9e9f6ca958f46c4bc32389ad78b
Eliseo Rocha <rochaeliseo91@gmail.com>
2018-10-29 16:00:18 -0700

    Initial commit

---
+++ ValidationSample/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ValidationSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FullNameTextBox = new System.Windows.Forms.TextBox();
+            this.AgeTextBox = new System.Windows.Forms.TextBox();
+            this.ValidateDataButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Full Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Age";
+            // 
+            // FullNameTextBox
+            // 
+            this.FullNameTextBox.Location = new System.Drawing.Point(193, 66);
+            this.FullNameTextBox.Name = "FullNameTextBox";
+            this.FullNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.FullNameTextBox.TabIndex = 2;
+            // 
+            // AgeTextBox
+            // 
+            this.AgeTextBox.Location = new System.Drawing.Point(193, 126);
+            this.AgeTextBox.Name = "AgeTextBox";
+            this.AgeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.AgeTextBox.TabIndex = 3;
+            // 
+            // ValidateDataButton
+            // 
+            this.ValidateDataButton.Location = new System.Drawing.Point(123, 223);
+            this.ValidateDataButton.Name = "ValidateDataButton";
+            this.ValidateDataButton.Size = new System.Drawing.Size(193, 84);
+            this.ValidateDataButton.TabIndex = 4;
+            this.ValidateDataButton.Text = "Validate Data";
+            this.ValidateDataButton.UseVisualStyleBackColor = true;
+            this.ValidateDataButton.Click += new System.EventHandler(this.ValidateDataButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 396);
+            this.Controls.Add(this.ValidateDataButton);
+            this.Controls.Add(this.AgeTextBox);
+            this.Controls.Add(this.FullNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox FullNameTextBox;
+        private System.Windows.Forms.TextBox AgeTextBox;
+        private System.Windows.Forms.Button ValidateDataButton;
+    }
+}
+
---
+++ ValidationSample/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidationSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ValidateDataButton_Click(object sender, EventArgs e)
+        {
+            if (IsValid())
+            {
+                MessageBox.Show("All good!");
+            }
+        }
+
+        private bool IsValid()
+        {
+            string name = FullNameTextBox.Text;
+            //Flag variable/boolean
+            bool isDataValid = true;
+            string errorMessages = "";
+
+            if (!IsPresent(name))
+            {
+                isDataValid = false;
+                errorMessages += "Name is required!\n"; // \n is a line break
+            }
+            if (!IsPresent(AgeTextBox) || !IsByte(AgeTextBox.Text))
+            {
+                isDataValid = false;
+                errorMessages += "Age is required and should be a number!\n";
+            }
+
+            if (IsByte(AgeTextBox.Text))
+            {
+                const byte minAge = 5;
+                const byte maxAge = 130;
+                byte age = Convert.ToByte(AgeTextBox.Text);
+                if (!IsWithinRange(minAge, maxAge, age))
+                {
+                    isDataValid = false;
+                    errorMessages += $"Age must be {minAge} - {maxAge}!";
+                }
+            }
+
+            if (!isDataValid)
+            {
+                MessageBox.Show(errorMessages);
+            }
+
+            return isDataValid;
+        }
+
+        /// <summary>
+        /// Checks to see if there is data in a texbox
+        /// </summary>
+        /// <returns></returns>
+        //private bool IsPresent(TextBox box)
+        //{
+        //    if (string.IsNullOrWhiteSpace(box.Text))
+        //    {
+        //        return false;
+        //    }
+        //    return true;
+        //}
+
+        //Same as above, use this so it isn't redundant with below
+        private bool IsPresent(TextBox box)
+        {
+            return IsPresent(box.Text);
+        }
+
+        private bool IsPresent(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Tests to ensure a number is within a range
+        /// </summary>
+        /// <param name="minValue">The inclusive lower bound</param>
+        /// <param name="maxValue">The inclusive upper bound</param>
+        /// <param name="numToTest">Number to test in the range</param>
+        /// <returns></returns>
+        private bool IsWithinRange(int minValue, int maxValue, int numToTest)
+        {
+            if (numToTest >= minValue && numToTest <= maxValue)
+            {
+                return true;
+            }
+            return false;
+        }
+        
+        private bool IsByte(string input)
+        {
+            //Exception handling same as bellow
+            byte test;
+            if (byte.TryParse(input, out test))
+            {
+                return true;
+            }
+            return false;
+
+            //Excetption handling
+            //try
+            //{
+            //    Convert.ToByte(input);
+            //    return true;
+            //}
+            //catch
+            //{
+            //    return false;
+            //}
+
+        }
+    }
+}
